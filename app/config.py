ae590e70e34e97c177628d412ef4ef5ef9c5e5ae
Danilo Vaccalluzzo <68116543+danilotrix86@users.noreply.github.com>
2024-08-13 15:31:13 +0200

    prompt improvement

--- app/config.py
+++ app/config.py
@@ -5,6 +5,6 @@ APPROACH = "ZEROSHOT" # "ZEROSHOT", "AGENTIC"
 LLM_ENGINE = "openai" # "openai", these models require a groq api key: "llama3-8b-8192", "llama3-70b-8192", "llama-3.1-70b-versatile", "llama-3.1-405b-reasoning" ONLY FOR PAYING MEMBERS,  "mixtral-8x7b-32768", "gemma-7b-it", "gemma2-9b-it". They are much cheaper than OpenAI's models.
 
 # if LLM_ENGINE = openai
-GPT_ENGINE = "gpt-3.5-turbo"  # gpt-4o, gpt-4o-mini or gpt-3.5-turbo
+GPT_ENGINE = "gpt-4o-mini"  # gpt-4o, gpt-4o-mini or gpt-3.5-turbo (VERY STUPID)
 
 LLM_TEMPERATURE = 0.2
