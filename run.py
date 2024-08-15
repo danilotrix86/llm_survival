1b535c27881d54fe7a929746b9f3edbb15b9fe20
Danilo Vaccalluzzo <68116543+danilotrix86@users.noreply.github.com>
2024-08-15 17:45:02 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,17 +1,10 @@
 import sys
 import uvicorn
-import os
-import logging
 
 # Add the app directory to the system path
 sys.path.append('./app')
 
 from app.main import app
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-
 if __name__ == "__main__":
-    # Log the PYTHONIOENCODING environment variable
-    logging.info(f"PYTHONIOENCODING: {os.getenv('PYTHONIOENCODING')}")
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
