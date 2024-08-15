9863fae08558b7920970292c60ca7d12d0e779fb
Danilo Vaccalluzzo <68116543+danilotrix86@users.noreply.github.com>
2024-08-15 17:37:12 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import sys
 import uvicorn
+import os
+import logging
 
 # Add the app directory to the system path
 sys.path.append('./app')
@@ -7,4 +9,5 @@ sys.path.append('./app')
 from app.main import app
 
 if __name__ == "__main__":
+    logging.INFO(os.getenv("PYTHONIOENCODING"))
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
